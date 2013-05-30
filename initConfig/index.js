5d8de88b4780c73f2b731429c0611b538b802df6
David Smith <codeactual@gmail.com>
2013-05-30 16:18:02 -0700

    fix(initConfig): Remove unused k/v pairs

--- initConfig/index.js
+++ initConfig/index.js
@@ -1,9 +1,5 @@
 module.exports = function() {
   'use strict';
 
-  this
-    .demand('mochaShelljsOpt', {stdout: true, stderr: false})
-    .demand('projName', 'gitemplate_name')
-    .demand('klassName', 'Gitemplate_klass')
-    .demand('instanceName', 'gitemplate_instance');
+  this.demand('mochaShelljsOpt', {stdout: true, stderr: false});
 };

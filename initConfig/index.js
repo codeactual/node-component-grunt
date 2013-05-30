1dc166428d49572e2752cd6a487a2060190700b8
David Smith <codeactual@gmail.com>
2013-05-30 00:14:17 -0700

    feat(initConfig): Add gitemplate variables
    Save the repeated assignments in all dependent gitemplate-* repos.

--- initConfig/index.js
+++ initConfig/index.js
@@ -1,5 +1,9 @@
 module.exports = function() {
   'use strict';
 
-  this.demand('mochaShelljsOpt', {stdout: true, stderr: false});
+  this
+    .demand('mochaShelljsOpt', {stdout: true, stderr: false})
+    .demand('projName', 'gitemplate_name')
+    .demand('klassName', 'Gitemplate_klass')
+    .demand('instanceName', 'gitemplate_instance');
 };

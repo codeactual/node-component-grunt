b43227f6eb33de2098ddb47b84d5ef792ff8826c
David Smith <codeactual@gmail.com>
2015-03-15 00:45:41 +0000

    chore(jshint): Continue eslint migration

--- initConfig/eslint.js
+++ initConfig/eslint.js
@@ -1,25 +1,18 @@
 module.exports = function() {
   'use strict';
 
-  var defOptions = {
-    node: true,
-    esnext: true
-  };
-
   return {
     src: {
-      options: defOptions,
       files: {
         src: ['index.js', 'lib/**/*.js']
       }
     },
     grunt: {
       files: {
-        src: ['Gruntfile.js'],
+        src: ['Gruntfile.js']
       }
     },
     tests: {
-      options: this.assimilate(defOptions, {expr: true}),
       files: {
         src: ['test/**/*.js']
       }

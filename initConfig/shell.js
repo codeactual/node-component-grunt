89732f1e8e4e713b433aa37be7f22a887daf75da
David Smith <codeactual@gmail.com>
2015-03-25 05:25:04 +0000

    fix(shell:test_lib): remove --harmony/--require co-mocha

--- initConfig/shell.js
+++ initConfig/shell.js
@@ -1,14 +1,7 @@
 module.exports = function(grunt) {
   'use strict';
 
-  var testLibCmd = 'mocha --harmony --colors --recursive --reporter spec test/lib';
-
-  if (this.learn('initConfig.harmony')) {
-    // Insert `--require co-mocha` before the last argument.
-    testLibCmd = testLibCmd.split(' ');
-    testLibCmd.splice(-1, 0, '--require', 'co-mocha');
-    testLibCmd = testLibCmd.join(' ');
-  }
+  var testLibCmd = 'mocha --colors --recursive --reporter spec test/lib';
 
   return {
     options: {
